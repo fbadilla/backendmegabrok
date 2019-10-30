1e6d94efd3aeeb4fe07a3621f1c483d36d26d393
fbadilla <36639028+fbadilla@users.noreply.github.com>
2019-10-30 10:12:09 -0300

    cambios

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.models import Account, AccountSerializer, Rol, RolSerializer, Reclamo, ReclamoSerializer, Evento, EventoSerializer, UserCreateSerializer
+from api.models import Account, AccountSerializer, Rol, RolSerializer, Reclamo, ReclamoSerializer, Documento, DocumentoSerializer, Evento, EventoSerializer, UserCreateSerializer
 
 
 from rest_framework.permissions import IsAuthenticated
