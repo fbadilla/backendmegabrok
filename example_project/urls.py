02394286370bc90fb1e002517a6c6a8a80d085ac
fbadilla <36639028+fbadilla@users.noreply.github.com>
2019-11-11 11:58:48 -0300

    cambios api, visualizacion de files

--- example_project/urls.py
+++ example_project/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework_swagger.views import get_swagger_view
+from django.conf import settings
+from django.conf.urls.static import static
 
-schema_view = get_swagger_view(title='API Boilerplate - 4Geeks Academy')
+schema_view = get_swagger_view(title='API Boilerplate ')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     url(r'^$', schema_view)
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
